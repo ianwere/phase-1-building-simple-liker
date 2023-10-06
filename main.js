338a63dc1079ddffe4ecf120047ca7ed36c26be7
Ian <ian.were@student.moringaschool.com>
2023-10-06 14:09:45 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
 
+  errorModal.classList.add("hidden"); // Add the .hidden class to hide the error modal initially
+
+  // Helper function to toggle the heart state and class
+  function toggleHeart(heart) {
+    if (heart.textContent === EMPTY_HEART) {
+      heart.textContent = FULL_HEART;
+      heart.classList.add("activated-heart");
+    } else {
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+    }
+  }
+
+  // Handle click events on the like buttons
+  document.querySelectorAll(".like").forEach((likeButton) => {
+    likeButton.addEventListener("click", (event) => {
+      const heart = event.target.querySelector(".like-glyph");
+
+      mimicServerCall()
+        .then(() => {
+          toggleHeart(heart);
+        })
+        .catch((error) => {
+          errorModal.classList.remove("hidden");
+          modalMessage.textContent = error;
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000); // Hide the modal after 3 seconds
+        });
+    });
+  });
+});
 
 
 
